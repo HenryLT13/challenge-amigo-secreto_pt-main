992da973c695bdb734481079702bba981a373747
Henrique Lianza <lianzahenrique5@gmail.com>
2025-01-28 18:47:44 -0300

    mais avanços

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,25 +1,50 @@
-//amigos e amigo sorteado
- let amigo
- let amigoSorteado
+let listaDeAmigos = [];
 
+let lista = document.getElementById("listaAmigos");
+lista.innerHTML = listaDeAmigos;
+
+
+
+//textos para tela
+function exibirTextoNaTela(tag, texto) {
+   let campo = document.querySelector(tag);
+      campo.innerHTML = texto;
+}
 
 
 
 //escolhendo amigo
- function adicionarAmigo() {
-if (amigoEscolhido == amigo) {
-   console.log('botão clicado')
-   console.log(amigo)
+function adicionarAmigo() {
+   let amigoEscolhido = document.querySelector('input').value;
+   if (amigoEscolhido == '') {
+      exibirTextoNaTela('h2', 'Por favor, insira um nome.')
+      limparCampo
+   } else if (listaDeAmigos.includes(amigoEscolhido)) {
+      exibirTextoNaTela('h2', 'Este nome já foi escolhido!');
+      limparCampo();
+   } else {
+      listaDeAmigos.push(amigoEscolhido);
+      exibirTextoNaTela('h2', 'Digite o nome dos seus amigos');
+      console.log(listaDeAmigos);
+      limparCampo();
+      return amigoEscolhido;
+   }
 }
+
+
+//limpar campo
+function limparCampo() {
+   amigoEscolhido = document.querySelector('input');
+   amigoEscolhido.value = '';
 }
 
 
-
-
-
-
-
-//sorteando
+//sorteando amigo
 function sortearAmigo() {
+   console.log('botão2');
+}
 
- }
+//lista de nomes de amigos
+function listarAmigos () {
+
+}
