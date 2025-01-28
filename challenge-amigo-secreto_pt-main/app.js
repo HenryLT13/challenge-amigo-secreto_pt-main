e25ed6e9fbc1c44b6c22b924365a84ac7f167192
Henrique Lianza <lianzahenrique5@gmail.com>
2025-01-28 18:47:31 -0300

    Mais avanços

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,25 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//amigos e amigo sorteado
+ let amigo
+ let amigoSorteado
+
+
+
+
+//escolhendo amigo
+ function adicionarAmigo() {
+if (amigoEscolhido == amigo) {
+   console.log('botão clicado')
+   console.log(amigo)
+}
+}
+
+
+
+
+
+
+
+//sorteando
+function sortearAmigo() {
+
+ }
